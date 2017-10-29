b1fe74dbbaed2ff5cc1a9a5874b55e7e3aba4526
Olegt0rr <t0rr@mail.ru>
2017-10-29 23:08:14 +0300

    Update aiodb.py

--- aiodb.py
+++ aiodb.py
@@ -13,33 +13,6 @@ logger.setLevel(logging.INFO)
 
 
 class DbHelper:
-    """
-    Class help to make clean primary code
-    It runs all queries, fetches, connects, disconnects, commits etc. within itself :)
-    Just use it like:
-
-            >>> from db import DbHelper
-            >>> db = DbHelper()
-
-        SELECT example - 1 (clear query)
-            >>> query = "SELECT * FROM table"
-            >>> result = db.fetchone(query)
-
-        SELECT example - 2 (query with data)
-            >>> query = "SELECT * FROM table WHERE some_column = %s"
-            >>> data = "your param"
-            >>> result = db.fetchone(query, data)
-
-        If you need to fetch some results from same query
-        use this case on first query:
-            >>> result = db.fetchone(query, data, open_cursor=True)
-        and this case on the next one:
-            >>> result = db.fetchone(query, data, next_result=True)
-        don' forget to close_cursor after all
-            >>> db.close_cursor()
-
-
-    """
 
     def __init__(self, loop=None):
         logger.debug("db: creating DbHelper instance")
